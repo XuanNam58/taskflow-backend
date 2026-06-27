4f48f770e06aee943f3be190262b46fc16631be4
XuanNam58 <104215250+XuanNam58@users.noreply.github.com>
2026-06-27 18:39:31 +0700

    feat(auth): add repositories, remove lombok annotations in entities, modify timestamp to timestamptz

---
+++ src/main/java/org/xuannam/taskflowbackend/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,4 @@
+package org.xuannam.taskflowbackend.auth.repository;
+
+public interface RefreshTokenRepository extends org.springframework.data.jpa.repository.JpaRepository<org.xuannam.taskflowbackend.auth.entity.RefreshTokenEntity, java.lang.Long> {
+  }
---
+++ src/main/java/org/xuannam/taskflowbackend/user/repository/UserRepository.java
@@ -0,0 +1,4 @@
+package org.xuannam.taskflowbackend.user.repository;
+
+public interface UserRepository extends org.springframework.data.jpa.repository.JpaRepository<org.xuannam.taskflowbackend.user.entity.UserEntity, java.lang.Long> {
+  }
