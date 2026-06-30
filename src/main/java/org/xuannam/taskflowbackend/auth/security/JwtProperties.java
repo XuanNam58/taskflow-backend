f62c85746b020e37454d1df61b6745a1e5f4d046
XuanNam58 <104215250+XuanNam58@users.noreply.github.com>
2026-06-30 22:57:50 +0700

    feat(auth): add jwt service configuration

---
+++ src/main/java/org/xuannam/taskflowbackend/auth/security/JwtProperties.java
@@ -0,0 +1,12 @@
+package org.xuannam.taskflowbackend.auth.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+@ConfigurationProperties(prefix = "app.jwt")
+public record JwtProperties(
+        String secret,
+        Duration accessTokenExpiration
+) {
+}
